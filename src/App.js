5550ec6ea4fba3a97e2d6e3709457d96583e564b
jch00047-hue <jch00047@mix.wvu.edu>
2026-05-08 11:09:09 -0400

    app update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import PortfolioGallery from "./components/PortfolioGallery";
 import Capabilities from "./components/Capabilities";
+import Coursework from "./components/Coursework";
+import Campaigns from "./components/Campaigns";
 import Volunteer from "./components/Volunteer";
 import Chargers from "./components/Chargers";
+import Press from "./components/Press";
 import Experience from "./components/Experience";
 import Marquee from "./components/Marquee";
 import Contact from "./components/Contact";
@@ -41,8 +44,11 @@ const Home = () => {
         <About />
         <PortfolioGallery />
         <Capabilities />
+        <Coursework />
+        <Campaigns />
         <Volunteer />
         <Chargers />
+        <Press />
         <Experience />
         <Contact />
       </main>
